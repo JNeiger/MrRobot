4cca04d469d47d576cd972b29cde277704ae4cec
Joseph Neiger <fcdneiger@gmail.com>
2018-04-13 18:01:55 -0400

    Update PCA9685.cpp

--- PCA9685.cpp
+++ PCA9685.cpp
@@ -103,8 +103,6 @@ void PCA9685::setPWMFreq(float freq) {
 void PCA9685::setPWM(uint8_t channel, float duty) {
 	uint16_t val = std::min((uint16_t)floor(duty * 4095 + 0.5), (uint16_t)4095);
 	
-	printf("Val %d\n\rDuty %f\n\r", val, duty);
-	
 	if (val == 4095) {
 		setPWM(channel, 4096, 0);
 	} else if (val == 0) {
